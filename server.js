3fc7b7c848531aa1670db8e98e2d5e081ac99b8b
RoyChen <roy94621@gmail.com>
2023-02-11 00:57:08 -0800

    add comment in server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import route from "./routes/bill.js";
 
+// Use dotenv to retrieve information from .env file
 dotenv.config();
 
 const app = express();
@@ -10,8 +11,10 @@ const port = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Define the entry point of server and usage of routing middleware
 app.use("/", route);
 
+// Run the server and listen on assigned port number
 app.listen(port, () => {
   console.log(`Server listening on port ${port}!`);
 });
