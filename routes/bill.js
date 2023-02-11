78ea01099645ffe10b01229e33d231baed29cf3f
RoyChen <roy94621@gmail.com>
2023-02-11 00:59:57 -0800

    add comment in bills.js

--- routes/bill.js
+++ routes/bill.js
@@ -3,7 +3,7 @@ import { getBills, postBill } from "../controllers/billController.js";
 
 const router = express.Router();
 
-router.get("/", getBills);
-router.post("/", postBill);
+router.get("/", getBills); // Call getBills when server get a GET request to "/"
+router.post("/", postBill); // Call postBill when server get a POST request to "/"
 
 export default router;
